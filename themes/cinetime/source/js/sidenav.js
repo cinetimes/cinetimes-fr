101a7becfd7e543908d1ff78b08acfd2499094c4
katre <contact@cinetimes.org>
2017-11-13 19:56:33 +0100

    fixed mobile sidnav width

--- themes/cinetime/source/js/sidenav.js
+++ themes/cinetime/source/js/sidenav.js
@@ -1,8 +1,8 @@
 /*Définis la largeur du side nav à 250px*/
 function openNav() {
   if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-    document.getElementById("mySidenav").style.width = "500px";
-    document.getElementById("main").style.marginLeft = "500px"; 
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px"; 
   } else {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
