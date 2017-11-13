1bb3f65ec638d0e56e263a455cbfb896d8125378
katre <contact@cinetimes.org>
2017-11-13 19:51:32 +0100

    created script to generate all posts in JSON format

---
+++ themes/cinetime/scripts/jsonify_posts.js
@@ -0,0 +1,23 @@
+'use strict';
+const util = require('hexo-util');
+
+hexo.extend.generator.register('json-content', site => {
+
+  var json = [];
+  // Iterate through all posts in the site saving the wanted data in an array of objects.
+  site.posts.forEach((post) => {
+    var item = {
+      // Default and custom variables
+			title: post.title,
+			author: post.author,
+			img: post.img_name,
+			url: post.permalink
+    };
+    json.push(item);
+  })
+
+	return {
+		path: 'content.json',
+		data: JSON.stringify(json)
+	}
+})
