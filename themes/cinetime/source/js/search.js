7a5e659ace1d683fe2356bf257c1d4a2d428f43a
katre <contact@cinetimes.org>
2018-02-11 01:06:13 +0100

    created searchbar fetching from TMDB API to fill most of the form fields

--- themes/cinetime/source/js/search.js
+++ themes/cinetime/source/js/search.js
@@ -2,14 +2,9 @@ $(function () {
 
   var medias = new Bloodhound({
     datumTokenizer: Bloodhound.tokenizers.obj.whitespace('title', 'author'),
-    // datumTokenizer: Bloodhound.tokenizers.obj.whitespace('author'),
     queryTokenizer: Bloodhound.tokenizers.whitespace,
-    // identify: function(obj) { return obj.title; },
-    // prefetch: '/content.json'
     local: movies
   });
-  // console.log('medias initialisated !');
-
 
   $('#q .typeahead').typeahead({
     highlight: true,
@@ -25,10 +20,67 @@ $(function () {
         '<div><a href="{{url}}"> {{title}} – {{author}}</div></a>'
       )
     }
-  // },
-  // {
-  //
-  // },
-  // {
+  });
+
+
+  var input = document.getElementById("tmdb-search");
+  // Let the filter be done by TMDB API.
+  var awesomeplete = new Awesomplete(input);
+
+  $('#tmdb-search').on('input', () => {
+    let value = document.getElementById("tmdb-search").value;
+    var request = new Request(`https://api.themoviedb.org/3/search/movie?include_adult=false&page=1&query=${value}&language=fr-FR&api_key=ef1989df4882fcb115198b492739c7d2`, {method: 'GET', mode: 'cors',})
+  
+    fetch(request)
+    .then(res => res.json())
+    .then(
+      (result) => {
+
+        var list = result.results.map((i) => {
+          return [i.title, i.id];
+          // return {label: i.title, value: i.id}
+        });
+
+        // Update the list to sort through
+        awesomeplete.list = list
+        // if the list contain less than five items, jsut show them all
+        list.length > 5 || (awesomeplete.filter = function(text, input) {
+           return true; 
+          })
+        awesomeplete.evaluate();
+      },
+      (error) => {
+        console.log(error.message)
+      }
+    )
+  });
+
+  document.getElementById("tmdb-search").addEventListener("awesomplete-select", (event) => {
+    console.log( event.text.label, event.text.value );
+    // Select all inputs i want to fill
+    var title = document.getElementById("input-title");
+    var original_title = document.getElementById("input-original_title");
+    var tmdb_id = document.getElementById("input-tmdb_id");
+    var author = document.getElementById("input-author");
+    var synopsis = document.getElementById("input-synopsis");
+    var img = document.getElementById("input-img");
+    var tags = document.getElementById("input-tags");
+    var release_date = document.getElementById("input-release_date");
+
+    fetch(`https://api.themoviedb.org/3/movie/${event.text.value}?api_key=ef1989df4882fcb115198b492739c7d2&language=fr-FR&append_to_response=credits`, {method:"GET"})
+    .then(res => res.json())
+    .then(
+      (result) => {
+        console.log(result);
+        title.value = result.title;
+        original_title.value = result.original_title;
+        synopsis.value = result.overview;
+        img.value = `https://image.tmdb.org/t/p/w500${result.poster_path}`;
+        author.value = result.credits.crew[0].name;
+        release_date.value = result.release_date;
+        tags.value = result.genres.map((genre) => {return genre.name});
+      }
+    )
   });
 });
+
