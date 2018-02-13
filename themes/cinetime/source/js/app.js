0ddc761993808ff87177c430ada0ad356d009afc
katre <contact@cinetimes.org>
2018-02-13 11:26:01 +0100

    add webtorrent play analytic event

--- themes/cinetime/source/js/app.js
+++ themes/cinetime/source/js/app.js
@@ -1,37 +1,48 @@
+// Lazy load images with the intersection observer
 (() => {
+	var images = document.querySelectorAll('.card-img-top, .lazy');
 
-    var images = document.querySelectorAll('.card-img-top, .lazy');
-
-    if(!('IntersectionObserver' in window)) {
-      images.forEach((image) => {
-        image.setAttribute("src",image.getAttribute('data-src'));
-      });
-    } else {
-      lazyLoadImages(images);
-    }
+	if (!('IntersectionObserver' in window)) {
+		images.forEach((image) => {
+			image.setAttribute("src", image.getAttribute('data-src'));
+		});
+	} else {
+		lazyLoadImages(images);
+	}
 
 })();
 
 function lazyLoadImages(images) {
-  var options = {
-    rootMargin: "50px 20px",
-    threshold: 0.01
-  };
+	var options = {
+		rootMargin: "50px 20px",
+		threshold: 0.01
+	};
 
-  var io = new IntersectionObserver((entries) => {
+	var io = new IntersectionObserver((entries) => {
 
-    entries.forEach(function(entry) {
-      if(entry.intersectionRatio <= 0 ) return;
-      // if(entry.target.hasAttribute("src")) return;
-      entry.target.setAttribute("src",entry.target.getAttribute('data-src'));
-      // console.log('src changed');
-      io.unobserve(entry.target);
-    });
-    },options);
+		entries.forEach(function (entry) {
+			if (entry.intersectionRatio <= 0) return;
+			// if(entry.target.hasAttribute("src")) return;
+			entry.target.setAttribute("src", entry.target.getAttribute('data-src'));
+			// console.log('src changed');
+			io.unobserve(entry.target);
+		});
+	}, options);
 
-  images.forEach((image) => {
-    io.observe(image);
-  });
-
-
+	images.forEach((image) => {
+		io.observe(image);
+	});
 }
+
+// Google Anlaytics Events
+(() => {
+	// Play webtorrent video
+	document.getElementById('video_webtorrent').onclick = () => {
+		ga('send', {
+			hitType: 'event',
+			eventCategory: 'Videos',
+			eventAction: 'play',
+			eventLabel: 'Webtorrent'
+		  });
+	} 
+})();
